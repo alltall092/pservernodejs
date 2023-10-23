a0fdc57b324e24384f8be48970447b6d1ae796d6
julio alberto perez <piyoyo279@gmail.com>
2023-10-22 21:54:07 -0400

    prueba

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,13 +2,13 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const db = new Sequelize({
-  database: process.env.DB_NAME || "api-archivo",
-  username: process.env.DB_USER || "julio",
-  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "bcxr6tautk5mcnovnjre",
+  username: process.env.DB_USER || "u8ewctaky9ynxod3",
+  host: process.env.DB_HOST || "bcxr6tautk5mcnovnjre-mysql.services.clever-cloud.com",
  // port: process.env.DB_PORT || 5432,
-  password: process.env.DB_PASSWORD || 120786,
-  port: process.env.DB_PORT || 5432, // Puerto predeterminado de PostgreSQL
-  dialect: "postgres", // Espe
+  password: process.env.DB_PASSWORD || Nf6QVYf1Q0dXi3nNhMck,
+  DB_PORT:3306,
+  dialect: "mysql",
   logging: false,
 });
 
