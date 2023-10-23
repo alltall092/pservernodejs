927ccda67422c4a13550cb4c90cde5534c73aad1
julio alberto perez <piyoyo279@gmail.com>
2023-10-22 22:07:47 -0400

    actualizar

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,8 +6,8 @@ const db = new Sequelize({
   username: process.env.DB_USER || "u8ewctaky9ynxod3",
   host: process.env.DB_HOST || "bcxr6tautk5mcnovnjre-mysql.services.clever-cloud.com",
  // port: process.env.DB_PORT || 5432,
-  password: process.env.DB_PASSWORD || Nf6QVYf1Q0dXi3nNhMck,
-  DB_PORT:3306,
+  password: process.env.DB_PASSWORD || "Nf6QVYf1Q0dXi3nNhMck",
+  port: process.env.DB_PORT ||3306,
   dialect: "mysql",
   logging: false,
 });
